48b017b7795744b82e4e89429f944b017b010385
kod3180 <kod3180@gmail.com>
2019-07-20 04:38:50 +0000

    messages list styled

--- src/components/messageItem.js
+++ src/components/messageItem.js
@@ -2,31 +2,46 @@ import React from 'react';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import DefaultUserImg from '../images/user.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const MessageItem = ({ date, profileImage, text, username, removeMessage, isCorrectUser }) => {
   return (
     <div>
       <li className="list-group-item">
-        <img
-          src={profileImage || DefaultUserImg}
-          alt={username}
-          height="100"
-          width="100"
-          className="timeline-image"
-        />
-        <div className="message-area">
-          <Link to="/">@{username} &nbsp;</Link>
-          <span className="text-muted">
-            <Moment className="text-muted" format="Do MMM YYYY">
-              {date}
-            </Moment>
-          </span>
-          <p>{text}</p>
+        <div className="list-item-info">
+          <img
+            src={profileImage || DefaultUserImg}
+            alt={username}
+            height="100"
+            width="100"
+            className="timeline-image"
+          />
+          <div className="message-area">
+            <Link to="/">@{username} &nbsp;</Link>
+            <p className="text-muted">
+              <Moment className="text-muted" format="Do MMM YYYY">
+                {date}
+              </Moment>
+            </p>
+          </div>
           {isCorrectUser && (
-            <a className="btn btn-danger" onClick={removeMessage}>
-              Delete
-            </a>
+            <span className="delete-btn" onClick={removeMessage}>
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </span>
           )}
+        </div>
+        <div>
+          <p>{text}</p>
+        </div>
+        <div>
+          <span>
+            <FontAwesomeIcon icon={faHeart} />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faComment} />
+          </span>
         </div>
       </li>
     </div>
